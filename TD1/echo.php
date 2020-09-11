babc86071c325415807501d90c2be0bdc71f1b7d
Guilhem Cros <guilhem.cros01@etu.umontpellier.fr>
2020-09-11 16:10:42 +0200

    Td1 exo5

--- TD1/echo.php
+++ TD1/echo.php
@@ -17,7 +17,65 @@
           $texte = "hello world !";
 
           // On écrit le contenu de la variable 'texte' dans la page Web
-          echo $texte;
+          echo $texte ."<br>";
+          
+          $prenom = "Marc";
+
+			echo "Bonjour " . $prenom;
+			echo "Bonjour $prenom";
+			echo 'Bonjour $prenom' . "<br>";
+
+			echo $prenom;
+			echo "$prenom <br>";
+
+      $marque = "BMW";
+      $couleur = "Bleu";
+      $immatriculation = "frt-503-yt";
+
+      echo "Voiture $immatriculation de marque $marque (couleur $couleur) <br>" ;
+      $voiture = array() ;
+      $voiture[ 'Marque' ] = $marque;
+      $voiture[ 'Couleur' ] = $couleur;
+      $voiture[ 'Immatriculation' ] = $immatriculation ;
+      print_r($voiture) ;
+      echo "<br>";
+      echo "Voiture $voiture[Immatriculation] de marque $voiture[Marque] (couleur $voiture[Couleur] ) <br>";
+
+      $voiture2 = array();
+      $voiture2[ 'Marque' ] = "Mercedes";
+      $voiture2[ 'Couleur' ] = "Noire";
+      $voiture2[ 'Immatriculation' ] = "dtr-845-ty" ;
+
+      $voiture3 = array();
+      $voiture3[ 'Marque' ] = "Dodge";
+      $voiture3[ 'Couleur' ] = "Verte";
+      $voiture3[ 'Immatriculation' ] = "tgy-854-bg";
+
+      $listeVoiture = array();
+      $listeVoiture['1'] = $voiture;
+      $listeVoiture['2'] = $voiture2;
+      $listeVoiture['3'] = $voiture3;
+      print_r($listeVoiture) ;
+      echo "<br>";
+
+      echo "<h2> Liste de voitures <br> </h2>";
+      if (empty($listeVoiture)) {
+          echo "La liste est vide" ;
+        }
+      else{
+        foreach ($listeVoiture as $key => $value) {
+          echo "<ul>";
+            foreach ($value as $key1 => $value1) {
+            echo "<li> $value1 </li>";
+            }
+          echo "</ul>";
+          }
+        }
+      
+
+      
+
+
         ?>
     </body>
 </html> 
