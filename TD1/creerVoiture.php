916a1128a4123c362bf8ca1773f505d54a76f489
Guilhem Cros <guilhem.cros01@etu.umontpellier.fr>
2020-09-18 13:02:32 +0200

    debut 9

---
+++ TD1/creerVoiture.php
@@ -0,0 +1,18 @@
+!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> Test voiture </title>
+    </head>
+
+   
+    <body>
+    	<?php
+
+
+    	
+
+    	?>
+   </body>
+
+</html>
