b03f28ea0c2421d00dbfb8740219c2ec9ce0acd8
Guilhem Cros <guilhem.cros01@etu.umontpellier.fr>
2020-09-18 15:45:52 +0200

    debut partie 2

---
+++ TD2/Model.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Model
+ *
+ * @author crosg
+ */
+require_once 'Conf.php';
+class Model {
+    
+    public static $pdo;
+    
+    
+    
+    
+    
+    public static function Init(){
+        $hostname = Conf::getHostname();
+        $database_name = Conf::getDatabase();
+        $login = Conf::getLogin();
+        $password = Conf::getPassword();
+       try{
+            self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password,
+                     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+   
+// On active le mode d'affichage des erreurs, et le lancement d'exception en cas d'erreur
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+        echo $e->getMessage(); // affiche un message d'erreur
+        die();
+        }
+        Model::Init();
+    }
+    
+    
+}
---
+++ TD2/TestModel.php
@@ -0,0 +1,8 @@
+<?php
+require_once "Model.php";
+echo "Connexion réussie !" ;
+?>
+
+
+
+
